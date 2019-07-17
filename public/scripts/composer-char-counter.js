dc790c54b51866ff92924bbdb317a2d77fb882b0
Nancy <webtech.pmp@gmail.com>
2019-07-16 18:24:09 -0700

    working code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
   // --- our code goes here ---
+  $('.new-tweet textarea').on('keyup', function (event) {
+    let char = $(this).val();
+    $('.counter').text(140 - char.length);
+    if (char.length > 140) {
+      $('.counter').css('color', '#ed2a07');
+    } else {
+      $('.counter').css('color', '#514519');
+    }
+  });
 });
