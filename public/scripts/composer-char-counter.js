ef3dd2e18f464802a74abc039a401119adbd686d
Nancy <webtech.pmp@gmail.com>
2019-07-17 08:32:08 -0700

    changed maxChar + overMax colour to vars for flexibility

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
   // --- our code goes here ---
   $('.new-tweet textarea').on('keyup', function (event) {
+    let overMax = #FF0000;
+    let maxChar = 140;
     let char = $(this).val();
-    $('.counter').text(140 - char.length);
-    if (char.length > 140) {
-      $('.counter').css('color', '#ed2a07');
+    $('.counter').text(maxChar - char.length);
+    if (char.length > maxChar) {
+      $('.counter').css('color', 'overMax');
     } else {
       $('.counter').css('color', '#514519');
     }
