f2775cdae82e5a563827d07badf4ed91bbbcd71e
Nancy <webtech.pmp@gmail.com>
2019-07-18 15:42:39 -0700

    fetch tweets work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,35 +51,36 @@ $(() => {
   };
 
   $("#create-New-Tweet").submit(function (event) {
-    $('twee')
-    console.log("Handler for .submit() called.");
     event.preventDefault();
+    $.post('/tweets', $(this).serialize(), (data, status) => {})
+    console.log($(this).serialize());
   });
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = [{
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
+  // const tweetData = [
+  //   // {
+  //   //   "user": {
+  //   //     "name": "Newton",
+  //   //     "avatars": "https://i.imgur.com/73hZDYK.png",
+  //   //     "handle": "@SirIsaac"
+  //   //   },
+  //   //   "content": {
+  //   //     "text": "If I have seen further it is by standing on the shoulders of giants"
+  //   //   },
+  //   //   "created_at": 1461116232227
+  //   // },
+  //   // {
+  //   //   "user": {
+  //   //     "name": "Descartes",
+  //   //     "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //   //     "handle": "@rd"
+  //   //   },
+  //   //   "content": {
+  //   //     "text": "Je pense , donc je suis"
+  //   //   },
+  //   //   "created_at": 1461113959088
+  //   // }
+  // ]
 
   // const $tweet = createTweetElement(tweetData);
 
@@ -93,5 +94,13 @@ $(() => {
       $('#all-Tweets').append($article);
     }
   }
-  renderTweets(tweetData);
+  //renderTweets(tweets);
+
+  const loadTweets = function () {
+    $.ajax('/tweets', {
+        method: 'GET'
+      })
+      .then(renderTweets);
+  }
+  loadTweets();
 });
