a7218de2e0c245c88ad1842acc59addf2accab25
Nancy <webtech.pmp@gmail.com>
2019-07-18 16:35:20 -0700

    valid text entry is <= 140 char

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -56,37 +56,20 @@ $(() => {
     console.log($(this).serialize());
   });
 
+  $('#create-New-Tweet').submit(function (event) {
+    event.preventDefault();
+    let txt = $('#new-Tweet-Text');
+    if (txt.val() === '' || txt.val() === 'null') {
+      alert('No tweet entered. Please enter text.')
+    } else {
+      if (txt.val().length > 140)
+        alert('Over 140 characters entered!')
+    }
+  });
+
+
   // Test / driver code (temporary). Eventually will get this from the server.
-  // const tweetData = [
-  //   // {
-  //   //   "user": {
-  //   //     "name": "Newton",
-  //   //     "avatars": "https://i.imgur.com/73hZDYK.png",
-  //   //     "handle": "@SirIsaac"
-  //   //   },
-  //   //   "content": {
-  //   //     "text": "If I have seen further it is by standing on the shoulders of giants"
-  //   //   },
-  //   //   "created_at": 1461116232227
-  //   // },
-  //   // {
-  //   //   "user": {
-  //   //     "name": "Descartes",
-  //   //     "avatars": "https://i.imgur.com/nlhLi3I.png",
-  //   //     "handle": "@rd"
-  //   //   },
-  //   //   "content": {
-  //   //     "text": "Je pense , donc je suis"
-  //   //   },
-  //   //   "created_at": 1461113959088
-  //   // }
-  // ]
 
-  // const $tweet = createTweetElement(tweetData);
-
-  // // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
   const renderTweets = function (tweets) {
     for (let tweet of tweets) {
