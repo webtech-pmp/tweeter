56bb6c90b58be124270ba63475726da43b2a6155
Nancy <webtech.pmp@gmail.com>
2019-07-17 20:24:33 -0700

    fix app JS

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -51,26 +51,47 @@ $(() => {
   };
 
   $("#create-New-Tweet").submit(function (event) {
+    $('twee')
     console.log("Handler for .submit() called.");
     event.preventDefault();
   });
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const tweetData = [{
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  // const $tweet = createTweetElement(tweetData);
+
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+  const renderTweets = function (tweets) {
+    for (let tweet of tweets) {
+      const $article = createTweetElement(tweet);
+      $('#all-Tweets').append($article);
+    }
   }
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets(tweetData);
 });
