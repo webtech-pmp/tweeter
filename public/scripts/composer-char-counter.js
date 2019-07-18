0ab85ad93b4437b32ea5fc5818799cca7ba6acba
Nancy <webtech.pmp@gmail.com>
2019-07-17 18:46:27 -0700

    fixed typo is composer-char-counter JS

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
   // --- our code goes here ---
   $('.new-tweet textarea').on('keyup', function (event) {
-    let overMax = #FF0000;
+    let overMax = '#FF0000';
     let maxChar = 140;
     let char = $(this).val();
     $('.counter').text(maxChar - char.length);
     if (char.length > maxChar) {
-      $('.counter').css('color', 'overMax');
+      $('.counter').css('color', overMax);
     } else {
       $('.counter').css('color', '#514519');
     }
