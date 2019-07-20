38fb57c2953a7847c3eb232a305a5c2cbb512096
Nancy <webtech.pmp@gmail.com>
2019-07-19 17:50:54 -0700

    add .hide: to hide error message if tweet length is valid

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-  // --- our code goes here ---
   $('.new-tweet textarea').on('keyup', function (event) {
     let overMax = '#FF0000';
     let maxChar = 140;
     let char = $(this).val();
+    if (char.length > 0 && char.length <= 140) {
+      $('.isa_error').hide();
+    }
     $('.counter').text(maxChar - char.length);
     if (char.length > maxChar) {
       $('.counter').css('color', overMax);
